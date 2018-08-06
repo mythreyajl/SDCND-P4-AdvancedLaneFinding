ab8d6f46632f7060cc2d9a9b1ed354508857e6de
mythreya <mythreya.jl@gmail.com>
2018-08-05 21:38:18 -0700

    Now works on a video stream
    The current pipeline works in detection mode on a video stream.

--- lane_detection.py
+++ lane_detection.py
@@ -4,75 +4,57 @@ import numpy as np
 import pickle
 import glob
 import matplotlib.pyplot as plt
+import argparse
+
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+
+
+# Global values
+# Data read from calibration script
+data = pickle.load(open("calibration.p", 'rb'))
+mtx = data["mtx"]
+dist = data["dist"]
+
+# Data collected after carefully looking at pixels of an undistorted image with straight lanes
+src = np.float32([[580, 460], [707, 460], [187, 720], [1125, 720]])
+dst = np.float32([[200, 0], [1000, 0], [200, 720], [1000, 720]])
+
+initialised = False
+
 
 class Lane:
+
     def __init__(self):
-        self.detected = False                           # was the line detected in the last iteration?
-        self.recent_xfitted = []                        # x values of the last n fits of the line
-        self.bestx = None                               # average x values of the fitted line over the last n iterations
-        self.best_fit = None                            # polynomial coefficients averaged over the last n iterations
-        self.current_fit = [np.array([False])]          # polynomial coefficients for the most recent fit
-        self.radius_of_curvature = None                 # radius of curvature of the line in some units
-        self.line_base_pos = None                       # distance in meters of vehicle center from the line
-        self.diffs = np.array([0,0,0], dtype='float')   # difference in fit coefficients between last and new fits
-        self.allx = None                                # x values for detected line pixels
-        self.ally = None                                # y values for detected line pixels
+
+        self.detected = False                               # was the line detected in the last iteration?
+        self.recent_xfitted = []                            # x values of the last n fits of the line
+        self.bestx = None                                   # avg x values of the fitted line over the last n iters
+        self.best_fit = None                                # polynomial coeffs averaged over the last n iterations
+        self.current_fit = [np.array([False])]              # polynomial coeffs for the most recent fit
+        self.radius_of_curvature = None                     # radius of curvature of the line in some units
+        self.line_base_pos = None                           # distance in meters of vehicle center from the line
+        self.diffs = np.array([0, 0, 0], dtype='float')     # difference in fit coefficients between last and new fits
+        self.allx = None                                    # x values for detected line pixels
+        self.ally = None                                    # y values for detected line pixels
 
 
-def abs_sobel_thresh(gray, orient='x', ksize=3, thresh=(0, 255)):
-    sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize)
-    if orient == 'y':
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=ksize)
-    abs_sobel = np.absolute(sobel)
-    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
-    grad_binary = np.zeros_like(gray)
-    grad_binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+def warp(img, src, dst):
 
-    return grad_binary
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    return warped, Minv
 
 
-def mag_thresh(gray, ksize=3, mag_thresh=(0, 255)):
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=ksize)
-    mag_sobel = np.sqrt(sobelx ** 2 + sobely ** 2)
-    scaled_sobel = np.uint8(255 * mag_sobel / np.max(mag_sobel))
-    mag_binary = np.zeros_like(scaled_sobel)
-    mag_binary[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 1
-
-    return mag_binary
-
-
-def dir_threshold(gray, ksize=3, thresh=(0, np.pi/2)):
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=ksize)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=ksize)
-    abs_sobelx = np.absolute(sobelx)
-    abs_sobely = np.absolute(sobely)
-    sobel_dir = np.arctan2(abs_sobely, abs_sobelx)
-    dir_binary = np.zeros_like(gray)
-    dir_binary[(sobel_dir >= thresh[0]) & (sobel_dir <= thresh[1])] = 1
-
-    return dir_binary
-
-
-def hls(img, thresh=(200, 255)):
-    # Saturation channel
-    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-    sat = hls[:, :, 2]
-    sat_binary = np.zeros_like(sat)
-    sat_binary[(sat > thresh[0]) & (sat <= thresh[1])] = 1
-
-    return sat_binary
-
-
-def pipeline(img,
-             s_thresh=(170, 255),
-             sx_thresh=(20, 100),
-             sy_thresh=(20, 100),
-             mag_thresh=(100, 255),
-             dir_thresh=(np.pi/3, np.pi/2)):
+def filter_image(img,
+                 s_thresh=(170, 255),
+                 sx_thresh=(20, 100),
+                 sy_thresh=(20, 100),
+                 mag_thresh=(100, 255),
+                 dir_thresh=(np.pi/3, np.pi/2)):
 
     img = np.copy(img)
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Convert to HLS color space and separate the V channel
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
@@ -112,16 +94,8 @@ def pipeline(img,
     # Stack each channel
     color_binary = np.dstack((sybinary, sxbinary, s_binary)) * 255
     combined = np.zeros_like(sybinary)
-    #combined[((sybinary == 1) & (sxbinary == 1)) | (s_binary == 1)] = 1
     combined[((sybinary == 1) & (sxbinary == 1)) | ((mag_binary == 1) & (dir_binary == 1)) | (s_binary == 1)] = 1
     return color_binary, combined
-
-
-def warp(img, src, dst):
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
-    Minv = cv2.getPerspectiveTransform(dst, src)
-    return warped, Minv
 
 
 def find_lane_pixels(binary_warped):
@@ -138,7 +112,7 @@ def find_lane_pixels(binary_warped):
     leftx_base = np.argmax(histogram[:midpoint])
     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-    # HYPERPARAMETERS
+    # Set parameters for window search
     nwindows = 9  # Choose the number of sliding windows
     margin = 100  # Set the width of the windows +/- margin
     minpix = 50   # Set minimum number of pixels found to recenter window
@@ -211,6 +185,7 @@ def find_lane_pixels(binary_warped):
 
 
 def fit_polynomial(binary_warped):
+
     # Find our lane pixels first
     leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
 
@@ -233,98 +208,86 @@ def fit_polynomial(binary_warped):
     out_img[lefty, leftx] = [255, 0, 0]
     out_img[righty, rightx] = [0, 0, 255]
 
-    #plt.plot(left_fitx, ploty, color='yellow')
-    #plt.plot(right_fitx, ploty, color='yellow')
-
-    return out_img, left_fitx, right_fitx, ploty
+    return out_img, left_fitx, right_fitx, ploty, leftx, lefty, rightx, righty
 
 
-def draw_lanes(image, warped, left_fitx, right_fitx, ploty, Minv):
+def draw_lanes(image, warped, left_fitx, right_fitx, ploty, Minv, leftx, lefty, rightx, righty):
+
+    # Prepare images to be used for drawing
     warp_zero = np.zeros_like(warped).astype(np.uint8)
-    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+    lane_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+    bounds_warp = lane_warp
 
     # Recast the x and y points into usable format for cv2.fillPoly()
     pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
     pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
     pts = np.hstack((pts_left, pts_right))
 
-    # Draw the lane onto the warped blank image
-    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+ # Draw the lane onto the warped blank image
+    cv2.fillPoly(lane_warp, np.int_([pts]), (0, 255, 0))
+    bounds_warp[lefty, leftx] = [0, 0, 255]
+    bounds_warp[righty, rightx] = [255, 0, 0]
 
     # Warp the blank back to original image space using inverse perspective matrix (Minv)
-    newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0]))
+    lane_warped = cv2.warpPerspective(lane_warp, Minv, (image.shape[1], image.shape[0]))
+    bounds_warped = cv2.warpPerspective(bounds_warp, Minv, (image.shape[1], image.shape[0]))
+
     # Combine the result with the original image
-    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    result = cv2.addWeighted(image, 1.0, lane_warped, 0.3, 0)
+    result = cv2.addWeighted(result, 0.5, bounds_warped, 0.3, 0)
 
     return result
 
 
+def detect_lanes(img, kernel=np.ones((3, 3), np.uint8), src=src, dst=dst):
+
+    # Undistort incoming image
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    # Filter and obtain best image for lane detection
+    color, comb = filter_image(undist)
+    closed_bin = cv2.morphologyEx(src=comb, op=cv2.MORPH_CLOSE, kernel=kernel)
+    opened_bin = cv2.morphologyEx(src=closed_bin, op=cv2.MORPH_OPEN, kernel=kernel)
+
+    # Warp the image according to previously decided image points
+    warped, Minv = warp(opened_bin, src, dst)
+
+    # Fit polynomials to warped points
+    fitted, lfx, rfx, ply, lx, ly, rx, ry = fit_polynomial(warped)
+
+    # Draw lane markings and lane area
+    overlaid = draw_lanes(image=undist, warped=warped, Minv=Minv, left_fitx=lfx, right_fitx=rfx, ploty=ply, leftx=lx,
+                          lefty=ly, rightx=rx, righty=ry)
+
+    return overlaid
+
+
+def track_lanes():
+
+    pass
+
+
 def find_lanes(img):
-    return None, None
+
+    return detect_lanes(img)
 
 
 if __name__ == "__main__":
-    data = pickle.load(open("calibration.p", 'rb'))
-    mtx = data["mtx"]
-    dist = data["dist"]
-    for filename in glob.glob("./undist_test_images/*jpg"):
-        # Read and apply undistortion
-        undist = cv2.imread(filename)
-        # undist = cv2.undistort(img, mtx, dist, None, mtx)
 
-        # Filters
-        k = 3
-        gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
-        sat_binary = hls(undist, thresh=(180, 255))
-        gradx = abs_sobel_thresh(gray, orient='x', ksize=k, thresh=(100, 255))
-        grady = abs_sobel_thresh(gray, orient='y', ksize=k, thresh=(100, 255))
-        mag_binary = mag_thresh(gray, ksize=k, mag_thresh=(100, 255))
-        dir_binary = dir_threshold(gray, ksize=k, thresh=(-np.pi / 2, np.pi / 2))
+    # Parse from command line
+    parser = argparse.ArgumentParser(description="Detect lane markers and drivable space from video")
+    parser.add_argument('-v', '--video', dest="video", help='Input file to be processed', required=True)
+    args = parser.parse_args()
 
-        # Add Morphological operations to the combined image
-        combined = np.zeros_like(dir_binary)
-        # combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1)) | (sat_binary == 1)] = 1
-        kernel = np.ones((3, 3), np.uint8)
-        opened_bin = cv2.morphologyEx(src=combined, op=cv2.MORPH_OPEN, kernel=kernel)
-        closed_bin = cv2.morphologyEx(src=combined, op=cv2.MORPH_CLOSE, kernel=kernel)
+    if not args.video:
+        print("Didn't provide an input video")
+        exit()
 
-        color, comb = pipeline(undist)
-        kernel = np.ones((3, 3), np.uint8)
-        closed_bin = cv2.morphologyEx(src=comb, op=cv2.MORPH_CLOSE, kernel=kernel)
-        opened_bin = cv2.morphologyEx(src=closed_bin, op=cv2.MORPH_OPEN, kernel=kernel)
+    vid = args.video
+    video_name = vid[:vid.rfind(".")]
 
-        # Warp binary image with mask extremities to make it orthographic
-        src = np.float32([[540, 490], [750, 490], [187, 720], [1125, 720]])
-        dst = np.float32([[200, 0], [1000, 0], [200, 720], [1000, 720]])
-        warped_img_a, _ = warp(closed_bin, src, dst)
-        warped_img_b, Minv = warp(opened_bin, src, dst)
-        warped_img_c, _= warp(comb, src, dst)
-
-        # Find pixels of interest on L&R and fit a polynomial
-        L, R = find_lanes(warped_img_c)
-
-        # Perspective change the polynomial in camera image
-        # Add markers as needed
-        # Distort image back to original camera
-        # For video, find_lanes changes
-
-        # Plot away
-        img_name = filename[filename.rfind('/')+1:]
-        # cv2.imshow('Closed: ' + img_name, 255*closed_bin)
-        # cv2.imshow('Opened: ' + img_name, 255*opened_bin)
-        # cv2.imshow('Satted: ' + img_name, 255*comb)
-        # cv2.imshow(img_name, 255 * warped_img_c)
-        # cv2.imshow('Closed: ' + img_name, 255*warped_img_a)
-        # cv2.imshow('Saturation: ' + img_name, 255*warped_img_c)
-        fitted, lfx, rfx, ply = fit_polynomial(warped_img_b)
-        out = draw_lanes(image=undist, warped=warped_img_b, Minv=Minv, left_fitx=lfx, right_fitx=rfx, ploty=ply)
-
-        #cv2.imshow('Unclosed: ' + img_name, 255 * warped_img_b)
-        #cv2.imshow('Fit: ' + img_name,  out)
-
-        out = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
-        plt.imshow(out)
-
-        plt.show()
-        plt.close()
-        #cv2.waitKey(0)
+    # Read Video
+    output = 'output_' + video_name + '.mp4'
+    clip1 = VideoFileClip(vid)
+    white_clip = clip1.fl_image(find_lanes)  # NOTE: this function expects color images!!
+    white_clip.write_videofile(output, audio=False)
